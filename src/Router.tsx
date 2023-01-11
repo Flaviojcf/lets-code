42aa294ca408f0df75499253df0f288eeb1898fc
Flavio <fjcf@poli.br>
2023-01-11 13:00:09 -0300

    feat: create new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { CardProductPage } from "./pages/CardProductPage";
 
 import { Home } from "./pages/Home/Home";
 import { MusicPlayer } from "./pages/MusicPlayer";
-
 
 export function Router() {
   return (
@@ -11,6 +11,7 @@ export function Router() {
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/challenges/week01" element={<MusicPlayer />} />
+        <Route path="/challenges/week02" element={<CardProductPage />} />
       </Route>
     </Routes>
   );
