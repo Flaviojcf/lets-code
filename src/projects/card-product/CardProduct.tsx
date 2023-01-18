dd209420fcdbd1b1d8d13a352abab3c2dfa6e702
Flavio <fjcf@poli.br>
2023-01-18 11:16:01 -0300

    feat: add 3d model to card Product

--- src/projects/card-product/CardProduct.tsx
+++ src/projects/card-product/CardProduct.tsx
@@ -16,13 +16,21 @@ export function CardProduct() {
     <Container>
       <Content>
         <CouchContainer>
-          <img
-            src={
-              !isRotating
-                ? "/assets/card-product/images/couch.png"
-                : "/assets/card-product/images/couchGif.gif"
-            }
-          />
+          {!isRotating ? (
+            <img src={"/assets/card-product/images/couch.png"} />
+          ) : (
+            <iframe
+              title="Margot 2 Seater Sofa, Old Rose Velvet"
+              height={300}
+              style={{border: "0px"}}
+              allow="autoplay; fullscreen; xr-spatial-tracking"
+              xr-spatial-tracking
+              execution-while-out-of-viewport
+              execution-while-not-rendered
+              web-share
+              src="https://sketchfab.com/models/b3d693a003c94f4e989c08fe08c3af8c/embed?autostart=1&camera=0&preload=1&transparent=1&ui_animations=0&ui_infos=0&ui_stop=0&ui_inspector=0&ui_watermark_link=0&ui_watermark=0&ui_hint=0&ui_ar=0&ui_help=0&ui_settings=0&ui_vr=0&ui_fullscreen=0&ui_annotations=0"
+            />
+          )}
           <img
             onClick={() => setIsRotating((state) => !state)}
             src={
